72211c2d6fa68b9fe6b13524db4467f64cff3f84
BugHunter-X <askzenai@gmail.com>
2025-02-20 11:16:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else if (a > b) {
+    return foo(a - b, b);
+  } else {
+    return foo(a, b - a);
+  }
+}
+
+console.log(foo(10, 2)); // this works fine 
+console.log(foo(10, 0)); // this will cause a stack overflow error
